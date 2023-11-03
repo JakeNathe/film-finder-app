8ca0f0957a683e542fee8bfef7d5815b5d619535
JakeNathe <Jakenathe1@gmail.com>
2023-11-03 13:23:05 -0500

    started movies page

--- src/pages/Movies.js
+++ src/pages/Movies.js
@@ -1,19 +1,51 @@
 import React from 'react';
+import { useState, useEffect } from "react";
+import axios from 'axios';
 import "./Pages.css";
+import IndividualContent from '../components/IndividualContent';
+import PageNumber from '../components/PageNumber';
+
+
 
 const Movies = () => {
+
+
+  
+  const [page, setPage] = useState(1);  // to return more than 1 page from api request
+  const [content, setContent] = useState([]);  // movie cont
+
+  const fetchMovies = async () => {
+    // fetch and destructure data
+    const {data} = await axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=true&include_video=false&page=${page}`);
+    // &with_genres=${genre}
+    setContent(data.results);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+    // stop annoying errors
+    // eslint-disable-next-line
+  }, [page]);
+
+
   return (
     <div>
         <span className="pageTitle">MOVIES</span>
-        <button>Action</button>
-        <button>Horror</button>
-        <button>Comedy</button>
-        <button>Romance</button>
-        <button>etc</button>
-        <button>Clear All Filters</button>
+        <div className="movies">
+          {content && content.map((c) => 
+          <IndividualContent 
+          key={c.id} 
+          id={c.id} 
+          poster={c.poster_path} 
+          title={c.title} 
+          media_type={c.media_type}
+          date={c.release_date}
+          vote_average={c.vote_average}
+          />)}
+        </div>
+        <PageNumber setPage={setPage}/>
     </div>
-    
-  )
-}
+  );
+};
 
 export default Movies;
--- src/pages/Popular.js
+++ src/pages/Popular.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import "./Pages.css";
-import IndividualContent from '../components/IndividualContent';
 import { useState, useEffect } from "react";
 import axios from "axios";
+import "./Pages.css";
+import IndividualContent from '../components/IndividualContent';
 import PageNumber from '../components/PageNumber';
 
 
 
 const Popular = () => {
   const [page, setPage] = useState(1);  // to return more than 1 page from api request
-  const [content, setContent] = useState([]);  // content form api
+  const [content, setContent] = useState([]);  // popular movie cont
 
   const fetchPopular = async () => {
     // fetch and destructure data
     const {data} = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`);
+    console.log(data);
     setContent(data.results);
   };
 
   useEffect(() => {
     fetchPopular();
+    // stop annoying errors
+    // eslint-disable-next-line
   }, [page]);
-
 
 
 // return individual movie/tvshow items with relevant data
@@ -28,7 +30,8 @@ const Popular = () => {
     <div>
         <span className="pageTitle">POPULAR NOW</span>
         <div className="popular">
-          {content && content.map((c) => <IndividualContent 
+          {content && content.map((c) => 
+          <IndividualContent 
           key={c.id} 
           id={c.id} 
           poster={c.poster_path} 
